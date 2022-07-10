9e56cedc573d5efa19b1e190aff16ab88c90793a
MarshyMelloow <76271349+MarshyMelloow@users.noreply.github.com>
2022-07-10 01:36:33 -0500

    Update exec.bat

--- app/exec.bat
+++ app/exec.bat
@@ -1,22 +1,21 @@
-echo on
+echo off
+git pull https://github.com/MarshyMelloow/fivemqul production
 color 4
 title Strting FiveM, Teamspeak, and Cad...
 color a 
-start "AlaskaPD Sonoran Cad.url"
+cd shortcuts
+start https://sonorancad.com/menu
 echo Exec Teamspeak C:\Program Files\TeamSpeak 3 Client\ts3client_win64.exe
 cd C:\Program Files\TeamSpeak 3 Client
 start ts3client_win64.exe
-color 4
-REM echo Exec Sornoran Cad
-REM cd  C:\Users\%USERNAME%\AppData\Local\Programs\sonorancad\
-REM start "Sonoran CAD.exe"
 color a
 echo Starting FiveM
-start C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.exe
+start https://cfx.re/join/mm567a
 echo Starting Inter Webs
-start C:\Users\%USERNAME%\Documents\TxAdmin.bat
+cd C:\FiveMAPD\app
+start TxAdmin.bat
 echo Success
-cd app/
+cd ..
+cd app
 cscript MessageBox.vbs "Script Success"
 execute C:\Users\%USERNAME%\Documents\TxAdmin.bat
-pause
