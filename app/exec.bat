e3d74db8bd5dc550df589269425ab595ce3c5d28
MarshyMelloow <76271349+MarshyMelloow@users.noreply.github.com>
2022-07-10 00:20:25 -0500

    Update exec.bat

--- app/exec.bat
+++ app/exec.bat
@@ -2,20 +2,21 @@ echo on
 color 4
 title Strting FiveM, Teamspeak, and Cad...
 color a 
+start "AlaskaPD Sonoran Cad.url"
 echo Exec Teamspeak C:\Program Files\TeamSpeak 3 Client\ts3client_win64.exe
 cd C:\Program Files\TeamSpeak 3 Client
 start ts3client_win64.exe
 color 4
-echo Exec Sornoran Cad
-REM cd C:\Users\%USERNAME%\Documents
-cd  C:\Users\%USERNAME%\AppData\Local\Programs\sonorancad\
-start "Sonoran CAD.exe"
+REM echo Exec Sornoran Cad
+REM cd  C:\Users\%USERNAME%\AppData\Local\Programs\sonorancad\
+REM start "Sonoran CAD.exe"
 color a
 echo Starting FiveM
 start C:\Users\%USERNAME%\AppData\Local\FiveM\FiveM.exe
 echo Starting Inter Webs
-REM start C:\Users\%USERNAME%\Documents\TxAdmin.bat
+start C:\Users\%USERNAME%\Documents\TxAdmin.bat
 echo Success
+cd app/
 cscript MessageBox.vbs "Script Success"
 execute C:\Users\%USERNAME%\Documents\TxAdmin.bat
 pause
