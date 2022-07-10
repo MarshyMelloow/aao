ff11f8c356dce51316d08c3b910e693e904e79bc
MarshyMelloow <76271349+MarshyMelloow@users.noreply.github.com>
2022-07-10 02:00:17 -0500

    Update install.bat

--- install/install.bat
+++ install/install.bat
@@ -4,3 +4,7 @@ echo 4
 echo This Script will install AAO
 pause
 winget install --id Git.Git -e --source winget
+cd C:\Users\%USERNAME%\
+git clone https://github.com/MarshyMelloow/aao/ production
+start C:\Users\%USERNAME%\
+exit
