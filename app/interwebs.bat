650a9effd5629300cc04d303a2b990a95f919354
MarshyMelloow <76271349+MarshyMelloow@users.noreply.github.com>
2022-07-12 23:22:56 -0500

    Update interwebs.bat

--- app/interwebs.bat
+++ app/interwebs.bat
@@ -1,7 +1,6 @@
 tite Launching...
 
 
- start "" http://game.alaskapd.us:30199/auth
  start "" https://docs.google.com/document/d/1pD5o5y0uMMZFYO4MNzKa_v4Fcmaq6XL3b-CRQz7RU-k/edit
  start "" https://docs.google.com/document/d/1UpHVwj6XA9ogh0MJWKq6Oik5fHfr445a6LQqgIazeH0/edit
  start "" https://docs.google.com/document/d/1mbTVt_YorIF0Rs4LPJ0xCmiKr98RXMUVx5K3mK67MSI/edit
