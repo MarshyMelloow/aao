4af816fa4d654e59181ffc9a6c32cbe145cfb765
MarshyMelloow <76271349+MarshyMelloow@users.noreply.github.com>
2022-08-03 22:15:11 -0500

    Update interwebs.bat

--- app/interwebs.bat
+++ app/interwebs.bat
@@ -1,8 +1,8 @@
 tite Launching...
 
 
- start "" https://docs.google.com/document/d/1pD5o5y0uMMZFYO4MNzKa_v4Fcmaq6XL3b-CRQz7RU-k/edit
- start "" https://docs.google.com/document/d/1UpHVwj6XA9ogh0MJWKq6Oik5fHfr445a6LQqgIazeH0/edit
- start "" https://docs.google.com/document/d/1mbTVt_YorIF0Rs4LPJ0xCmiKr98RXMUVx5K3mK67MSI/edit
- start "" https://docs.google.com/spreadsheets/d/1L3sYD5fGejoOj6s3A8FSf2mdN9I8U_PwUtjQfe0kt-U/edit#gid=1205100156
+
+start "" https://docs.google.com/document/d/1UpHVwj6XA9ogh0MJWKq6Oik5fHfr445a6LQqgIazeH0/edit
+start "" https://docs.google.com/document/d/1mbTVt_YorIF0Rs4LPJ0xCmiKr98RXMUVx5K3mK67MSI/edit
+start "" https://docs.google.com/spreadsheets/d/1L3sYD5fGejoOj6s3A8FSf2mdN9I8U_PwUtjQfe0kt-U/edit#gid=1205100156
 exit
